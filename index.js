a4514eff876f1976dd007af3b4ddc9bc03fc732b
ode-to-the-code <justcallmethis@gmail.com>
2017-08-23 16:15:34 -0400

    Done.

--- index.js
+++ index.js
@@ -11,10 +11,21 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
+ var itemName = item
+ var itemPrice = Math.round(Math.random() * (100 - 1) + 1);
+ var itemObject = {[itemName]: itemPrice }
+ cart.push(itemObject)
+ console.log(`${itemName} has been added to your cart.`)
+ return cart
 }
 
 function viewCart() {
   // write your code here
+  if (cart.length === 0) {
+    console.log('Your shopping cart is empty.')
+  } else if (cart.length === 1) {
+    console.log(`In your cart, you have lemons at ${cart[0]}.`)
+  }
 }
 
 function total() {
